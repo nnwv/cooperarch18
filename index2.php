0dcbc994251ecc5ad971662b7d20727e9ba263c3
nnwv <29682995+nnwv@users.noreply.github.com>
2022-11-06 16:38:40 -0500

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,1 +1,43 @@
 
+
+
+
+<html>
+<head>
+<title>chris coy for jstchillin</title>
+<style type="text/css">
+html {margin:0; padding-left:25px; padding-top:15px; padding-bottom: 25px; line-height:18px; }
+p {color:#030303; font-family:"Lucida Console", Monaco, monospace; font-size:12px; line-height:18px;}
+p.header { font-family:"Times New Roman", Times, serif; font-size:16px; line-height:18px; font-weight:800; }
+p.secondary {color:#777; font-family:"Lucida Console", Monaco, monospace; font-size:12px; font-style:italic; line-height:18px;}
+p.tertiary {color:#777; font-family:"Lucida Console", Monaco, monospace; font-size:12px; line-height:18px;}
+a:link { color:#00f; text-decoration:underline; }
+a:visited { color:#00f; text-decoration:underline; }
+a:hover { color:#000; text-decoration:underline; }
+a:active { color:#00f; text-decoration:underline; }
+a.secondary:link { color:#777; text-decoration:underline; }
+a.secondary:visited { color:#777; text-decoration:underline; }
+</style>
+</head>
+	<body>
+        <table width="860" border="0">
+  <tr>
+    <td valign="top" width="420"><p>"So, more than a substance, plastic is the very idea of its infinite transformation; as its everyday name indicates, it is ubiquity made visible... it is less a thing than the trace of a movement." -Roland Barthes<br />
+<br />What of the pixel then? If plastic maintains some sort of material presence, a pixel is the ghost of a trace. It is a liquid crystal in a constant state of flux, suspended like a digital spectre- an electronic mirage. The pixelated, screen-based Image is one that carries with it a material history of milliseconds: existing for the infinitely configurable, immediate moment.</p></td>
+    <td width="20"></td>
+    <td valign="top" width="420"><p class="header">The Image has been altered 617 times.</p>
+		<p><a href='image.php?id=1667228611.jpg'>Download</a> the current version.</p>
+        <form action='index.php' enctype="multipart/form-data" method='post'>
+			<p>Replace:</p>
+			<input type='hidden' name='submission' value='1' />
+			<input type='file' name='file' accept='image/gif,image/jpeg,image/png' />
+			<input type='submit' value='Upload' />
+		</form>
+<p><a href='all.php'>View all</a> versions.</p>
+</td>
+  </tr>
+</table><br><br>	
+		<img src='img/1667228611.jpg'><br><br>
+        <p class="secondary">a project by <a href="http://seecoy.com" target="_blank" class="secondary">Chris Coy</a></span> for <a href="http://jstchillin.org/home.html" target="_blank" class="secondary">jstchillin</a>, with assistance from <a href="mailto:codysblanchard@gmail.com" class="secondary">Cody Blanchard</a></p>
+	</body>
+</html>
